1de5d4b876978a28b7318d87e5b98caa91cbaaf9
YOUSSEF <joe_1999_2010@hotmail.com>
2019-07-15 18:36:50 +0200

    updated and added more files

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from flask import Flask, render_template, url_for, redirect, flash
 from flask_wtf.csrf import CSRFProtect
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 import forms
 import models
@@ -9,6 +11,9 @@ import models
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'never been easier'
 csrf = CSRFProtect(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
@@ -19,11 +24,11 @@ def index():
 def signup():
     form = forms.NewUserForm()
     if form.validate_on_submit():
-        user = models.User.create(username = form.username.data)
+        user = models.User.create(username=form.username.data)
         flash(f'{user.username.capitalize()} your data has been saved in my database!')
         return redirect(url_for('hello', name=user.username))
     context= {
-        'form':form,
+        'form': form,
         'current_time': datetime.utcnow()
     }
     return render_template('signup.html', **context)
