77fd19090b81da6b68517c63cb2231d7925f6e8b
YOUSSEF <joe_1999_2010@hotmail.com>
2019-07-23 19:43:16 +0200

    added login.html and admin_page.html amended base.html and index.html modified app.py and forms.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import date, datetime, timedelta
 import os
 
-from flask import Flask, render_template, url_for, redirect, flash
+from flask import Flask, jsonify, render_template, url_for, redirect, flash
 from flask_wtf.csrf import CSRFProtect
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -20,18 +20,18 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    __table__name = 'users'
+    __table_name__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(50))
 
 
 class Comment(db.Model):
-    __table__name = 'comments'
+    __table_name__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime(), default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
     text = db.Column(db.String(500))
-    is_approved = db.Column(db.Boolean(), default=False, nullable=False)
+    is_approved = db.Column(db.Boolean, default=False, nullable=False)
 
 @app.before_first_request
 def before_first_request():
@@ -40,9 +40,26 @@ def before_first_request():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
+    # db.session.add(Comment(text='bla bla blo'))
+    # db.session.commit()
+
     form = forms.NewComment()
+    in7days = date.today() - timedelta(days=7)
+    in1month = date.today() - timedelta(days=30)
+    in1year = date.today() - timedelta(days=365)
+    database_today = Comment.query.filter(Comment.date >= date.today()).all()
+    database_last_week = Comment.query.filter(Comment.date < date.today(), Comment.date >= in7days).all()
+    database_last_month = Comment.query.filter(Comment.date < in7days, Comment.date >= in1month).all()
+    database_last_year = Comment.query.filter(Comment.date < in1month, Comment.date >= in1year).all()
+    database_past = Comment.query.filter(Comment.date < in1year).all()
     context = {
-        'form':form
+        'form':form,
+        'database_today':database_today,
+        'database_last_week':database_last_week,
+        'database_last_month':database_last_month,
+        'database_last_year':database_last_year,
+        'database_past':database_past
     }
     if form.validate_on_submit():
         comment = Comment(text=form.comment.data)
@@ -50,3 +67,22 @@ def index():
         db.session.commit()
         return redirect(url_for('index', **context))
     return render_template('index.html', **context)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = forms.LoginForm()
+    context = {'form':form}
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+        if not user:
+            flash('Wrong username or password')
+            return render_template('login.html', **context)
+        else:
+            return redirect(url_for('admin_page'))
+    return render_template('login.html', **context)
+
+
+# @app.route('/admin/<name>', methods=['GET', 'POST'])
+# def admin_page(name):
+#     removeform = forms.AdminRemoveForm()
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, validators
+from wtforms import StringField, SubmitField, validators, PasswordField, SelectField
 
 
 class NewComment(FlaskForm):
@@ -9,3 +9,19 @@ class NewComment(FlaskForm):
         validators.InputRequired()
         ])
     post = SubmitField('Post')
+
+
+class Subscribe(FlaskForm):
+    gmail = SubmitField('Subscribe on Gmail')
+    #messenger = SubmitField('Subscribe on Messenger')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', [validators.Length(max=20, min=3), validators.InputRequired()])
+    password = PasswordField('Password', [validators.InputRequired()])
+    submit = SubmitField('Login')
+
+
+class AdminRemoveForm(FlaskForm):
+    comment = SelectField(f'comment: ')
+    submit = SubmitField('Remove')
